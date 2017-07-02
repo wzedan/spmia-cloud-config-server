f3fb4f21598a8118ab88ef4a7939c3828f39922d
wz <wzedan@gmail.com>
2017-07-02 12:21:33 +0300

    Initial commit

---
+++ src/main/java/wz/learning/io/SpmiaCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package wz.learning.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpmiaCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpmiaCloudConfigServerApplication.class, args);
+	}
+}
